142d0657f6f8c8755f4addcfd83f444ecb34c058
cooky0235 <cooky0236@126.com>
2026-07-20 20:35:54 +0900

    Update result.js

--- functions/api/result.js
+++ functions/api/result.js
@@ -1,25 +1,56 @@
-export async function onRequestPost(context){
-
-    const data = await context.request.json();
-
-    await context.env.DB.prepare(
-        `
-        INSERT INTO results
-        (name,test_date,total,correct,rate)
-        VALUES (?,?,?,?,?)
-        `
-    )
-    .bind(
-        data.name,
-        data.date,
-        data.total,
-        data.correct,
-        data.rate
-    )
-    .run();
+export async function onRequest(context) {
 
 
-    return Response.json({
-        success:true
-    });
+    // 查询成绩
+    if(context.request.method === "GET"){
+
+
+        const result = await context.env.DB
+        .prepare(
+            `
+            SELECT *
+            FROM results
+            ORDER BY id DESC
+            `
+        )
+        .all();
+
+
+        return Response.json(result.results);
+
+    }
+
+
+
+    // 保存成绩
+    if(context.request.method === "POST"){
+
+
+        const data = await context.request.json();
+
+
+        await context.env.DB.prepare(
+            `
+            INSERT INTO results
+            (name, test_date, total, correct, rate)
+            VALUES (?, ?, ?, ?, ?)
+            `
+        )
+        .bind(
+            data.name,
+            data.date,
+            data.total,
+            data.correct,
+            data.rate
+        )
+        .run();
+
+
+        return Response.json({
+            success:true
+        });
+
+    }
+
+
 }
