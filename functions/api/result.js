9b5603eed2c72ed29c2901ecded01779ae4f0a22
cooky0235 <cooky0236@126.com>
2026-07-20 20:03:55 +0900

    Update result.js

--- functions/api/result.js
+++ functions/api/result.js
@@ -1,1 +1,25 @@
+export async function onRequestPost(context){
 
+    const data = await context.request.json();
+
+    await context.env.DB.prepare(
+        `
+        INSERT INTO results
+        (name,test_date,total,correct,rate)
+        VALUES (?,?,?,?,?)
+        `
+    )
+    .bind(
+        data.name,
+        data.date,
+        data.total,
+        data.correct,
+        data.rate
+    )
+    .run();
+
+
+    return Response.json({
+        success:true
+    });
+}
