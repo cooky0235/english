add0385b83a79b2c294610bd069230079bed048a
cooky0235 <cooky0236@126.com>
2026-07-23 23:50:01 +0900

    Update words.js

--- functions/api/words.js
+++ functions/api/words.js
@@ -1,10 +1,20 @@
 export async function onRequest(context) {
 
+
     const url = new URL(context.request.url);
 
-    const group = Number(url.searchParams.get("group"));
 
-    const result = await env.DB.prepare(
+    const start =
+    Number(url.searchParams.get("start"));
+
+
+    const end =
+    Number(url.searchParams.get("end"));
+
+
+
+    const result =
+    await context.env.DB.prepare(
 `
 SELECT *
 FROM words
@@ -14,6 +24,7 @@ ORDER BY id
 )
 .bind(start,end)
 .all();
+
 
     return Response.json(result.results);
 
